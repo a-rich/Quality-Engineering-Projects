6d1a3298b56045ffc5b18d673cb767325230a666
Alex <alexrichards92@me.com>
2018-02-09 19:19:25 -0800

    renamed trianglesWithTestCases.c to triangles and aligned comments for test cases

--- hw_1/triangles.c
+++ hw_1/triangles.c
@@ -7,11 +7,10 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <limits.h>
 
-#define ARGS(args) args[0], args[1], args[2]
+#define EXPAND_ARGS(args) args[0], args[1], args[2]
 
 int find_largest_side(int a, int b, int c, int * other_sides) {
     /*
@@ -19,7 +18,7 @@ int find_largest_side(int a, int b, int c, int * other_sides) {
      * copying them into an array, and then returns the largest line.
      */
 
-    int largest = (a > b && a > c ) ? a : (b > c ? b : c);
+    int largest = (a > b && a >c ) ? a : (b > c ? b : c);
     memcpy(other_sides,
             (a == largest) ? (int [2]){b, c} :
                     ((b == largest) ? (int [2]){a, c} : (int [2]){a, b}),
@@ -57,23 +56,25 @@ int main() {
      * Prints whether or not the sides form a triangle for each test.
      */
 
-    int tests[4][3] = {
-        {2, 3, 5},
-        {2, 4, 5},
-        {2, 5, 5},
-        {5, 5, 5}
+    int tests[10][3] = {
+      {3, 4, 4},        // isosceles
+      {2, 3, 4},        // scalene
+      {4, 4, 4},        // equilateral
+      {2, 3, 5},        // fail since 5 = 2 + 3
+      {1, 3, 5},        // fail since 5 > 1 + 3
+      {-1, 5, 4},       // fail since negative
+      {-3, -2, 7},      // fail since negative
+      {-2, -3, -4},     // fail since negative
+      {2, 3, INT_MIN},  // fail
+      {3, INT_MAX, 5},  // fail
     };
 
-    for (int i=0; i < sizeof(tests)/sizeof(tests[0]); i++) {
-
-        if (is_triangle(ARGS(tests[i])))
+    for (int i=0; i < sizeof(tests)/sizeof(tests[0]); i++)
+        if (is_triangle(EXPAND_ARGS(tests[i])))
             printf("Sides %d, %d, and %d form a%s triangle.\n",
-                    ARGS(tests[i]),
-                    triangle_type(ARGS(tests[i])));
+                    EXPAND_ARGS(tests[i]),
+                    triangle_type(EXPAND_ARGS(tests[i])));
         else
             printf("Sides %d, %d, and %d do not form a triangle.\n",
-                    ARGS(tests[i]));
-    }
-
-    return 0;
+                    EXPAND_ARGS(tests[i]));
 }
