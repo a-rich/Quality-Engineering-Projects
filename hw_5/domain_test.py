06755373e37321e3ec8e9b2392691cff6070aa67
Alex <alexrichards92@me.com>
2018-03-06 21:20:08 -0800

    fixed conditionals and added preliminary image of domain thing

--- hw_5/domain_test.py
+++ hw_5/domain_test.py
@@ -1,16 +1,15 @@
 def some_unit(x, y):
     a = 0
 
-    if x and y:
-        a = (x*y)**(x-y)
-    elif x and not y:
-        a = (x*(y+1))**(x-y)
-    elif not x and y:
-        a = ((x+1)*y)**(x-y)
+    if x:                             # P1
+        if y:                         # P2
+            a = (x*y)**(x-y)
+        else:
+            a = (x*(y+1))**(x-y)
     else:
-        a = ((x+1)*(y+1))**(x-y)
+        if y:
+            a = ((x+1)*y)**(x-y)
+        else:
+            a = ((x+1)*(y+1))**(x-y)
 
-    if a > 0:
-        return 'Positive'
-    else:
-        return 'Non-positive'
+    return a
